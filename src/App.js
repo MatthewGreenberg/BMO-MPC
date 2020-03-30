be0a25c58993a1fff5f748f69ae273f5ec925201
Matthew Greenberg <matthew.greenberg+1@lvthn.com>
2020-03-30 16:26:43 -0500

    add more gl options

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
         camera={{
           position: [0, 0, 13],
         }}
-        gl={{ antialias: true }}
+        gl={{ alpha: false, antialias: false, logarithmicDepthBuffer: true }}
         pixelRatio={window.devicePixelRatio}
         onCreated={({ gl, scene }) => {
           gl.toneMapping = THREE.ACESFilmicToneMapping
